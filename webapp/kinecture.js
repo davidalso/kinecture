7bf2e533abcedb5824c7bbd861a97dcebabdf22a
Gregory Rose <greg@ncroses.com>
2014-11-10 19:23:02 -0500

    v1 meteor d3

--- webapp/kinecture.js
+++ webapp/kinecture.js
@@ -1,6 +1,3 @@
-// simple-todos.js
-Tasks = new Mongo.Collection("tasks");
-
 Kinects = new Mongo.Collection("kinects");
 
 if (Meteor.isClient) {
@@ -8,60 +5,159 @@ if (Meteor.isClient) {
   Template.body.helpers({
     kinects: function() {
       return Kinects.find({});
-    },
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-    },
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-    },
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
     }
+  });
+
+  Template.kinect.helpers({
+    left: function() {
+      return Session.get("left") == this._id;
+    },
+    right: function() {
+      return Session.get("right") == this._id;
+    }
+  });
+
+  Template.kinect.events({
+    "click .setleft": function() {
+      Session.set("left", this._id);
+    },
+
+    "click .setright": function() {
+      Session.set("right", this._id);
+    },
   });
 
   Template.body.events({
-    "submit .new-task": function (event) {
-      // This function is called when the new task form is submitted
-
-      var text = event.target.text.value;
-
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
-
-      // Prevent default form submit
-      return false;
-    },
-    // Add to Template.body.events
-    "change .hide-completed input": function (event) {
-      Session.set("hideCompleted", event.target.checked);
-    },
-
     "click #deleteEverything": function() {
       Meteor.call("deleteEverything");
+    },
+
+    "click #randomize": function() {
+      var cursor = Kinects.find({});
+      if (!cursor.count()) return;
+
+      cursor.forEach(function (row) {
+          var silence = Math.random() < 0.5;
+          Kinects.update(
+            row._id,
+            {"$set":
+              {
+                angle: Math.random() * 100.0 - 50.0,
+                timestamp: new Date(),
+                confidence: Math.random(),
+                loudness: Math.random(),
+                speech: !silence && Math.random() < 0.5,
+                custom_speech: !silence && Math.random() < 0.5,
+                silence: silence,
+                bins: [1,2,3,4]
+              }
+            }
+          );
+      });
     }
   });
 
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
-  });
+  Template.scatterPlot.rendered = function(){
+    //Width and height
+    var w = 500;
+    var h = 300;
+    var padding = 30;
+
+    //Create scale functions
+    var xScale = d3.scale.linear()
+               .range([padding, w - padding * 2]);
+
+    var yScale = d3.scale.linear()
+               .range([h - padding, padding]);
+
+    //Define X axis
+    var xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom")
+            .ticks(5);
+
+    //Define Y axis
+    var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left")
+            .ticks(5);
+
+    //Create SVG element
+    var svg = d3.select("#scatterPlot")
+          .attr("width", w)
+          .attr("height", h);
+
+    //Define key function, to be used when binding data
+    var key = function(d) {
+      return d._id;
+    };
+
+    //Create X axis
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (h - padding) + ")");
+
+    //Create Y axis
+    svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + padding + ",0)");
+
+    Deps.autorun(function(){
+      var dataset = [{_id: 1, x: 2, y: 2}, {_id: 2, x: 8, y :2}];
+
+      //= Points.find({},{fields:{x:1,y:1}}).fetch();
+
+      //Update scale domains
+      xScale.domain([0, d3.max(dataset, function(d) { return d.x; })]);
+      yScale.domain([0, d3.max(dataset, function(d) { return d.y; })]);
+
+      //Update X axis
+      svg.select(".x.axis")
+        .transition()
+        .duration(1000)
+        .call(xAxis);
+
+      //Update Y axis
+      svg.select(".y.axis")
+        .transition()
+        .duration(1000)
+        .call(yAxis);
+
+
+      var circles = svg
+        .selectAll("circle")
+        .data(dataset, key);
+
+      //Create
+      circles
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+          return xScale(d.x);
+        })
+        .attr("cy", function(d) {
+          return yScale(d.y);
+        })
+        .attr("r", 2);
+
+      //Update
+      circles
+        .transition()
+        .duration(1000)
+        .attr("cx", function(d) {
+          return xScale(d.x);
+        })
+        .attr("cy", function(d) {
+          return yScale(d.y);
+        });
+
+      //Remove
+      circles
+        .exit()
+        .remove();
+    });
+
+  };
 }
 
 if (Meteor.isServer) {
@@ -70,7 +166,7 @@ if (Meteor.isServer) {
 
       Kinects.update(
         {name: this.query.name},
-        this.query,
+        {$set: this.query},
         {upsert: true}
       );
 
